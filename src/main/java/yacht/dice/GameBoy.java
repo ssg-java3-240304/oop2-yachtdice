b43d9240d638e4150621deecd2e6bfd809252229
jeonhyeonseon <good_pretty@naver.com>
2024-04-28 20:53:56 +0900

    Update: GameBoy, GameManager주석 추가

--- src/main/java/yacht/dice/GameBoy.java
+++ src/main/java/yacht/dice/GameBoy.java
@@ -2,7 +2,7 @@ package yacht.dice;
 
 public class GameBoy {
     public static void main(String[] args) {
-        GameManager gameManager = new GameManager();
-        gameManager.start();
+        GameManager gameManager = new GameManager(); // gameManager라는 객체 생성
+        gameManager.start(); // gameManager에서 start라는 메소드 호출
     }
 }
