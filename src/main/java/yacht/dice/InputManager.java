cfe56e480de1249103aed0c3062ba63a3836d3ba
cstangga <cstangga92@gmail.com>
2024-05-01 16:32:53 +0900

    feat : 주사위index확인, a~l 점수판 명렁어 확인

--- src/main/java/yacht/dice/InputManager.java
+++ src/main/java/yacht/dice/InputManager.java
@@ -1,27 +1,100 @@
 package yacht.dice;
 
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class InputManager {
-    // 게임스타트, 주사위를 던진다 or 스코어 보드에 점수를 넣는다
-    Scanner sc=new Scanner(System.in);
-    String input =sc.next();
-    public void InputManager()
-    {
-        System.out.printf("선택을 해주세요 1: GAMESTART, 2: 주사위 , 3: 점수창");
-        InputType status=InputType.inputType(input);
-        switch (status) {
-            case GAMESTART -> {
-                System.out.printf("게임을 재시작 합니다"); // 게임 스타트(리셋??)
-            }
-            case ROLL_DICE -> {
-                System.out.printf("굴릴 주사위를 선택");// 굴릴 주사위 선택b
-            }
-            case CHOOSE_SCORE_BORAD -> {
-                System.out.printf("안녕하세요");//스코어 보드로 이론
-                Scoreboard scoreboard=new Scoreboard();
-//                scoreboard.Scoreprint();
+    Scanner sc = new Scanner(System.in);
+
+    public String status(String input) {
+        while (true) {
+            if (input.equals("r"))
+                break;
+            else if (input.equals("w"))
+                break;
+            else {
+                System.out.printf("잘못된 입력입니다 다시 입력해주세요 : ");
+                input = sc.nextLine();
             }
         }
+        return input;
+    }
+
+    public String[] checkIndex(String inputIndex) {
+        String[] indexStrSplit = inputIndex.split(" ");
+
+        for (int i = 0; i < indexStrSplit.length; i++)// 인덱스별로 for문 들어가서
+        {
+            while (true) {
+                // 1~6사이에 없다면 다시 입력을 받고
+                if (Integer.parseInt(indexStrSplit[i]) < 1 || Integer.parseInt(indexStrSplit[i]) > 6) {
+
+                    System.out.printf("%d번째의 위치의 값이 잘못된 입력 값입니다\n", i + 1);
+                    System.out.printf("1~6 사이의 값을 입력해주세요 : ");
+                    indexStrSplit[i] = sc.nextLine();
+
+                } else// 1~6 사이 값이라면 while문 탈출
+                    break;
+            }
+        }
+        return indexStrSplit;
+    }
+
+    public String CheckScoreChoice(char checkchoice) {
+        String choice=null;
+        do {
+            switch (checkchoice) {
+                case 'a':
+                    System.out.println("Aces 이동");
+                    choice = "Aces";
+                    return choice;
+                case 'b':
+                    System.out.println("Deuces 이동");
+                    choice = "Deuces";
+                    return choice;
+                case 'c':
+                    System.out.println("Threes 이동");
+                    choice = "Threes";
+                    return choice;
+                case 'd':
+                    System.out.println("Fours 이동");
+                    choice = "Fours";
+                    return choice;
+                case 'e':
+                    System.out.println("Fives 이동");
+                    choice = "Fives";
+                    return choice;
+                case 'f':
+                    System.out.println("Sixes 이동");
+                    choice = "Sixes";
+                    return choice;
+                case 'g':
+                    System.out.println("Choice 이동");
+                    choice = "Choice";
+                    return choice;
+                case 'h':
+                    System.out.println("4 of a kind 이동");
+                    choice = "4 of a kind";
+                    return choice;
+                case 'i':
+                    System.out.println("Full House 이동");
+                    choice = "Full House";
+                    return choice;
+                case 'j':
+                    System.out.println("S.Straight 이동");
+                    choice = "S.Straight";
+                    return choice;
+                case 'k':
+                    System.out.println("L.Straight 이동");
+                    choice = "L.straight";
+                    return choice;
+                case 'l':
+                    System.out.println("Yacht 이동");
+                    choice = "Yacht";
+                    return choice;
+            }
+            System.out.printf("잘못입력했습니다 a~l 사이의 값을 입력해주세요 : ");
+            checkchoice = sc.nextLine().charAt(0);
+        } while (true);
     }
 }
