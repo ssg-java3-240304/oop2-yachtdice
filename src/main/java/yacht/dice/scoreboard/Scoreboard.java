7cd4694ace4b08245c0f5f2a0ce6f95892c148f7
hurryduck <tigeryck0812@gmail.com>
2024-05-01 19:22:48 +0900

    Update: Scoreboard 작업 완료

--- src/main/java/yacht/dice/scoreboard/Scoreboard.java
+++ src/main/java/yacht/dice/scoreboard/Scoreboard.java
@@ -2,20 +2,36 @@ package yacht.dice.scoreboard;
 
 public class Scoreboard {
     public final int SECTION_LENGTH = ScoreboardSectionType.values().length;
-    private Integer[] sectionScore;
+    private final Integer[] SECTION_SCORE;
 
     public Scoreboard() {
-        this.sectionScore = new Integer[SECTION_LENGTH];
-        this.sectionScore[ScoreboardSectionType.SUM.getIndex()] = 0;
-        this.sectionScore[ScoreboardSectionType.BONUS.getIndex()] = 0;
-        this.sectionScore[ScoreboardSectionType.TOTAL.getIndex()] = 0;
+        this.SECTION_SCORE = new Integer[SECTION_LENGTH];
+        this.SECTION_SCORE[ScoreboardSectionType.SUM.getIndex()] = 0;
+        this.SECTION_SCORE[ScoreboardSectionType.BONUS.getIndex()] = 0;
+        this.SECTION_SCORE[ScoreboardSectionType.TOTAL.getIndex()] = 0;
     }
 
     public void setSectionScore(ScoreboardSectionType sectionType, int score) {
-        this.sectionScore[sectionType.getIndex()] = score;
+        this.SECTION_SCORE[sectionType.getIndex()] = score;
     }
 
-    public Integer[] getSectionScore() {
-        return sectionScore.clone();
+    public Integer[] getSECTION_SCORE() {
+        return SECTION_SCORE.clone();
+    }
+
+    public int getUpperSectionScore() {
+        int total = 0;
+        for (int i = ScoreboardSectionType.ONES.getIndex(); i <= ScoreboardSectionType.SIXES.getIndex(); i++) {
+            total += SECTION_SCORE[i];
+        }
+        return total;
+    }
+
+    public int getLowerSectionScore() {
+        int total = 0;
+        for (int i = ScoreboardSectionType.THREE_OF_A_KIND.getIndex(); i <= ScoreboardSectionType.CHANCE.getIndex(); i++) {
+            total += SECTION_SCORE[i];
+        }
+        return getUpperSectionScore()+ total;
     }
 }
--- src/main/java/yacht/dice/scoreboard/ScoreboardManager.java
+++ src/main/java/yacht/dice/scoreboard/ScoreboardManager.java
@@ -1,18 +1,21 @@
 package yacht.dice.scoreboard;
 
+import yacht.dice.DiceList;
+
 public class ScoreboardManager {
     // FFFFFFFFFFFFFFFFFFFField
-    private Scoreboard scoreboard;
+    private final Scoreboard SCOREBOARD;
 
     // CCCCCCCCCCCCCCCCCCCConstructor
     public ScoreboardManager() {
-        this.scoreboard = new Scoreboard();
+        this.SCOREBOARD = new Scoreboard();
     }
 
-    public String[] getScoreboard(int[] dice) {
-        Integer[] sectionScore = scoreboard.getSectionScore();
-        String[] scoreboardStr = new String[scoreboard.SECTION_LENGTH];
-        for (int i = 0; i < scoreboard.SECTION_LENGTH; i++) {
+    // MMMMMMMMMMMMMMMMMMMMethod
+    public String[] getScoreboard(DiceList dice) {
+        Integer[] sectionScore = SCOREBOARD.getSECTION_SCORE();
+        String[] scoreboardStr = new String[SCOREBOARD.SECTION_LENGTH];
+        for (int i = 0; i < SCOREBOARD.SECTION_LENGTH; i++) {
             if (sectionScore[i] != null) {
                 scoreboardStr[i] = sectionScore[i].toString();
             }
@@ -23,38 +26,103 @@ public class ScoreboardManager {
         return scoreboardStr;
     }
 
-    // MMMMMMMMMMMMMMMMMMMMethod
-    public void  setSection(ScoreboardSectionType sectionType, int[] dice) {
-        scoreboard.setSectionScore(sectionType, getSectionResult(sectionType, dice));
+    public void  setSection(ScoreboardSectionType sectionType, DiceList dice) {
+        SCOREBOARD.setSectionScore(sectionType, getSectionResult(sectionType, dice));
     }
 
-    private int getSectionResult(ScoreboardSectionType sectionType, int[] dice) {
+    private int getSectionResult(ScoreboardSectionType sectionType, DiceList dice) {
         return switch (sectionType) {
             // Upper section
             case ONES -> getResultOfOnes(dice);
-            case TWOS -> getResultOfOnes(dice);
-            case THREES -> getResultOfOnes(dice);
-            case FOURS -> getResultOfOnes(dice);
-            case FIVES -> getResultOfOnes(dice);
-            case SIXES -> getResultOfOnes(dice);
+            case TWOS -> getResultOfTwos(dice);
+            case THREES -> getResultOfThrees(dice);
+            case FOURS -> getResultOfFours(dice);
+            case FIVES -> getResultOfFives(dice);
+            case SIXES -> getResultOfSixes(dice);
             // Sub-counting
-            case SUM -> getResultOfOnes(dice);
-            case BONUS -> getResultOfOnes(dice);
+            case SUM -> getSum();
+            case BONUS -> getBonus();
 
             // Lower section
-            case THREE_OF_A_KIND -> getResultOfOnes(dice);
-            case FOUR_OF_A_KIND -> getResultOfOnes(dice);
-            case FULL_HOUSE -> getResultOfOnes(dice);
-            case SMALL_STRAIGHT -> getResultOfOnes(dice);
-            case LARGE_STRAIGHT -> getResultOfOnes(dice);
-            case YAHTZEE -> getResultOfOnes(dice);
-            case CHANCE -> getResultOfOnes(dice);
+            case THREE_OF_A_KIND -> getResultOfThreeKind(dice);
+            case FOUR_OF_A_KIND -> getResultOfFourKind(dice);
+            case FULL_HOUSE -> getResultOfFullHouse(dice);
+            case SMALL_STRAIGHT -> getResultOfSmallStraight(dice);
+            case LARGE_STRAIGHT -> getResultOfLargeStraight(dice);
+            case YAHTZEE -> getResultOfYahtzee(dice);
+            case CHANCE -> getResultOfChance(dice);
             // Total-counting
-            case TOTAL -> getResultOfOnes(dice);
+            case TOTAL -> getTotal();
         };
     }
 
-    private int getResultOfOnes(int[] dice) {
+    // Lower section
+    private int getResultOfOnes(DiceList dice) {return dice.count(1);}
+    private int getResultOfTwos(DiceList dice) {return dice.count(2) * 2;}
+    private int getResultOfThrees(DiceList dice) {return dice.count(3 * 3);}
+    private int getResultOfFours(DiceList dice) {return dice.count(4) * 4;}
+    private int getResultOfFives(DiceList dice) {return dice.count(5) * 5;}
+    private int getResultOfSixes(DiceList dice) {return dice.count(6) * 6;}
+
+    private int getSum() {
+        return SCOREBOARD.getUpperSectionScore();
+    }
+
+    private int getBonus() {
+        if (SCOREBOARD.getUpperSectionScore() >= 63) {
+            return 35;
+        }
         return 0;
+    }
+
+    // Lower section
+    private int getResultOfThreeKind(DiceList dice) {
+        if (dice.isNumberOfKind(3)) {
+            return dice.sum();
+        }
+        return 0;
+    }
+
+    private int getResultOfFourKind(DiceList dice) {
+        if (dice.isNumberOfKind(4)) {
+            return dice.sum();
+        }
+        return 0;
+    }
+
+    private int getResultOfFullHouse(DiceList dice) {
+        if (dice.isFullHouse()) {
+            return dice.sum();
+        }
+        return 0;
+    }
+
+    private int getResultOfSmallStraight(DiceList dice) {
+        if (dice.isStraight(4)) {
+            return 15;
+        }
+        return 0;
+    }
+
+    private int getResultOfLargeStraight(DiceList dice) {
+        if (dice.isStraight(5)) {
+            return 30;
+        }
+        return 0;
+    }
+
+    private int getResultOfYahtzee(DiceList dice) {
+        if (dice.isNumberOfKind(5)) {
+            return 50;
+        }
+        return 0;
+    }
+
+    private int getResultOfChance(DiceList dice) {
+        return dice.sum();
+    }
+
+    private int getTotal() {
+        return SCOREBOARD.getUpperSectionScore() + SCOREBOARD.getLowerSectionScore();
     }
 }
