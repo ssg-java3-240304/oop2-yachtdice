fbc4c2214541a513c867b814cdc177ca3a4defff
hurryduck <tigeryck0812@gmail.com>
2024-05-01 19:23:22 +0900

    Update: gamemanager변경

--- src/main/java/yacht/dice/GameManager.java
+++ src/main/java/yacht/dice/GameManager.java
@@ -1,38 +1,37 @@
 package yacht.dice;
 
-import yacht.dice.scoreboard.Scoreboard;
 import yacht.dice.scoreboard.ScoreboardManager;
 import yacht.dice.scoreboard.ScoreboardSectionType;
 
 import java.util.Scanner;
 
 public class GameManager {
-    private static final int ROUND = 12;        // 총 라운드 = 12
-    private int currentRound;                   // 현재 라운드
-    private static final int ROLL_CHANCE = 2;   // 앞으로 남은 라운드 횟수 = 2
-    private int currentRollChance;              // 현재 주사위를 굴릴 수 있는 남은 기회
-    private ScoreboardManager scoreboardManager;              // 점수판
-    private int[] dice;                         // 주사위 배열
+    private final int ROUND = 12;            // 총 라운드 = 12
+    private final int ROLL_CHANCE = 2;       // 주사위를 구릴 수 있는 기회
+    private final ScoreboardManager SCOREBOARD_MANAGER;    // 점수판
+    private final DiceList DICE;                              // 주사위 배열
 
     private Scanner sc = new Scanner(System.in);
 
     public GameManager() {
-        scoreboardManager = new ScoreboardManager(); // scoreboard(점수판) 객체 생성
-        dice = new int[6]; // 주사위 객체에 6이라는 크기 지정
+        this.SCOREBOARD_MANAGER = new ScoreboardManager(); // scoreboard(점수판) 객체 생성
+        this.DICE = new DiceList(); // 주사위 객체에 6이라는 크기 지정
     }
 
     // start 메소드에서 게임 라운드가 남아있을 때 주사위를 던져 출력하게 한다.
     public void start() {
-        for (int i = 0; i < 1; i++) {
+        for (int i = 0; i < this.ROUND; i++) {
+            // 현재 주사위를 굴릴 수 있는 남은 기회
+            int currentRollChance = 2;
+            rollAllDice();
             printScoreboard(); // 점수판 출력
-
-            this.currentRollChance = 2;
-            rollAllDice(); // 주사위를 다시 던진다
             printAllDice(); // 굴린 주사위를 출력
 
-            if (this.currentRollChance > 0) { // 만약 주사위를 굴릴 수 있는 기회가 있다면
+            if (currentRollChance > 0) { // 만약 주사위를 굴릴 수 있는 기회가 있다면
                 if (isChoiceRollDice()) { // 선택한 주사위가 있다면 -> isChoiceRollDice 메소드 호출
-                    this.currentRollChance--; // 남은 라운드는 감소
+                    currentRollChance--; // 남은 라운드는 감소
+
+
                     System.out.print("give me index: ");
                     String inputStr = sc.nextLine(); // 2 3 5 // String 타입에 변수 inputStr 를 인덱스로 받는다.
                     rerollDiceAtIndex(inputStr); // inputStr 를 매개변수로 받은 rerollDiceAtIndex 메소드를 호출한다.
@@ -48,10 +47,10 @@ public class GameManager {
         }
     }
 
-    // dice에 1~6까지 랜덤한 숫자가 들어가 있게 한다.
+    // dice 에 1~6까지 랜덤한 숫자가 들어가 있게 한다.
     private void rollAllDice() {
-        for (int i = 0; i < this.dice.length; i++) {
-            this.dice[i] = (int) (Math.random() * 6) + 1; // 1 ~ 6
+        for (int i = 0; i < this.DICE.length(); i++) {
+            this.DICE.set(i, (int) (Math.random() * 6) + 1); // 1 ~ 6
         }
     }
 
@@ -59,7 +58,7 @@ public class GameManager {
     private void rerollDiceAtIndex(String indexStr) {
         String[] indexStrSplit = indexStr.split(" ");
         for (String index : indexStrSplit) {
-            this.dice[Integer.valueOf(index) - 1] = (int) (Math.random() * 6) + 1; // this.dice[각 요소를 정수로 변환하고 -1]
+            this.DICE.set(Integer.valueOf(index) - 1, (int) (Math.random() * 6) + 1);
         }
     }
 
@@ -68,20 +67,31 @@ public class GameManager {
     // ====================
     // 점수판 출력
     private void printScoreboard() {
-        String[] scoreboardString = scoreboardManager.getScoreboard(dice);
-        for (int i = 0; i < scoreboardString.length; i++) {
-            System.out.print(ScoreboardSectionType.getByIndex(i).getName() + " ");
+        String[] scoreboardString = SCOREBOARD_MANAGER.getScoreboard(DICE);
+        System.out.println("------------------------------------------------------------------------------------------------------------------------------------------------");
+        for (int i = 0; i < scoreboardString.length / 2; i++) {
+            System.out.printf("%-17s|", ScoreboardSectionType.getByIndex(i).getName());
         }
         System.out.println();
-        for (int i = 0; i < scoreboardString.length; i++) {
-            System.out.print(scoreboardString[i] + "\t");
+        for (int i = 0; i < scoreboardString.length / 2; i++) {
+            System.out.printf("%-17s|", scoreboardString[i]);
         }
         System.out.println();
+        System.out.println("------------------------------------------------------------------------------------------------------------------------------------------------");
+        for (int i = 8; i < scoreboardString.length; i++) {
+            System.out.printf("%-17s|", ScoreboardSectionType.getByIndex(i).getName());
+        }
+        System.out.println();
+        for (int i = 8; i < scoreboardString.length; i++) {
+            System.out.printf("%-17s|", scoreboardString[i]);
+        }
+        System.out.println();
+        System.out.println("------------------------------------------------------------------------------------------------------------------------------------------------");
     }
 
     // dice 객체 배열이 die 라는 변수에 값을 할당하여 굴린 주사위를 출력한다.
     private void printAllDice() {
-        for (int die : this.dice) {
+        for (int die : this.DICE.getList()) {
             System.out.print(die + " ");
         }
         System.out.println();
